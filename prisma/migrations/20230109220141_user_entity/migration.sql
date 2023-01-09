71d2de9fc2e2cc3c44cfd91b82d74d45d20b7df8
sCarita <gustavosilvacarita@gmail.com>
2023-01-09 23:55:49 +0000

    data: created initial schema for our jwt auth api backend.

---
+++ prisma/migrations/20230109220141_user_entity/migration.sql
@@ -0,0 +1,29 @@
+-- CreateEnum
+CREATE TYPE "RoleEnumType" AS ENUM ('user', 'admin');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "email" TEXT NOT NULL,
+    "verified" BOOLEAN DEFAULT false,
+    "password" TEXT NOT NULL,
+    "role" "RoleEnumType" DEFAULT 'user',
+    "verificationCode" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_verificationCode_key" ON "users"("verificationCode");
+
+-- CreateIndex
+CREATE INDEX "users_email_verificationCode_idx" ON "users"("email", "verificationCode");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_verificationCode_key" ON "users"("email", "verificationCode");
